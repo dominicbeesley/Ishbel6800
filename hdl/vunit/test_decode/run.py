a77b057eaf1ff11d3a33d8a93b56941b7923ad7b
dominicbeesley <8866586+dominicbeesley@users.noreply.github.com>
2026-04-20 15:16:54 +0100

    remove John's core

--- hdl/vunit/test_decode/run.py
+++ hdl/vunit/test_decode/run.py
@@ -12,7 +12,6 @@ lib = vu.add_library("dossy_6800")
 
 # Add all files ending in .vhd in current working directory to library
 lib.add_source_files("./*.vhd")
-lib.add_source_files("../../library/3rdparty/JohnKent/cpu68_dom.vhd")
 lib.add_source_files("../../library/mine/*.vhd")
 lib.add_source_files("../../library/mine/dossy_6800/*.vhd")
 lib.add_source_files("../../cards/*.vhd")
