d97e8b752535662793fe055d9d220d6c120cd705
dominicbeesley <8866586+dominicbeesley@users.noreply.github.com>
2026-07-15 14:04:53 +0100

    Initialise belt and braces - run clock during reset

--- hdl/library/mine/max7219_matrix.vhd
+++ hdl/library/mine/max7219_matrix.vhd
@@ -95,7 +95,7 @@ architecture rtl of max7219_matrix is
 	constant MAX7219_CMD_SHUTDOWN			: std_logic_vector(7 downto 0) := x"0C";
 	constant MAX7219_CMD_TEST				: std_logic_vector(7 downto 0) := x"0F";
 
-	type t_state is (init, run);
+	type t_state is (reset1, reset2, reset3, init, run);
 	type t_row_state is (start, shifting);
 	type t_bit_state is (phase0, phase1, phase2, phase3);
 
@@ -113,6 +113,7 @@ architecture rtl of max7219_matrix is
 begin
 
 	p_row:process(RST_I, CLK_I, CLKEN_I)
+	variable v_load : boolean;
 	begin
 		
 		if RST_I = '1' then
@@ -121,10 +122,12 @@ begin
 			dat_ix <= (others => '0');
 			bit_ix <= (others => '1');
 			r_row_state <= start;
-			r_state <= init;
+			r_state <= reset1;
 			sr <= (others => '0');
 		elsif rising_edge(CLK_I) then
 			if CLKEN_I = '1' then
+
+				v_load := false;
 
 				if r_bit_state = phase1 then
 					case r_row_state is
@@ -134,7 +137,15 @@ begin
 							if row_ix = ROWS then
 								row_ix <= to_unsigned(1, row_ix'length);
 								dat_ix <= (others => '0');
-								r_state <= run;
+								if r_state = reset1 then
+									r_state <= reset2;
+								elsif r_state = reset2 then
+									r_state <= reset3;
+								elsif r_state = reset3 then
+									r_state <= init;
+								else
+									r_state <= run;
+								end if;
 							else
 								row_ix <= row_ix + 1;
 							end if;
@@ -149,6 +160,7 @@ begin
 								r_row_state <= start;
 								col_ix <= (others => '0');
 							else
+								v_load := true;
 								bit_ix <= (others => '0');
 								col_ix <= col_ix + 1;
 							end if;
@@ -157,22 +169,27 @@ begin
 							bit_ix <= bit_ix + 1;
 						end if;
 					else
+						v_load := true;
 						bit_ix <= (others => '0');
 						r_row_state <= shifting;
 					end if;
 				end if;
 
 
-				if r_bit_state = phase2 and bit_ix = 15 then
-					if r_state = init then
+				if v_load then
+					if r_state = reset1 or r_state = reset2 or r_state = reset3 then
+						sr <= (others => '0');
+					elsif r_state = init then
 						case to_integer(row_ix) is 
-							when 2 =>
+							when 2 | 7 =>
+								sr <= MAX7219_CMD_TEST & "00000000";
+							when 1 | 3 =>
 								sr <= MAX7219_CMD_SHUTDOWN & "00000001";
-							when 3 =>
-								sr <= MAX7219_CMD_INTENSITY & "00000000";
 							when 4 =>
-								sr <= MAX7219_CMD_SCANLIMIT & std_logic_vector(to_unsigned(ROWS - 1, 8));
+								sr <= MAX7219_CMD_INTENSITY & "00000001";
 							when 5 =>
+								sr <= MAX7219_CMD_SCANLIMIT & std_logic_vector(to_unsigned(ROWS - 1, 8));
+							when 6 =>
 								sr <= MAX7219_CMD_DECODEMODE & "00000000";
 							when others =>
 								sr <= (others => '0');
@@ -188,26 +205,13 @@ begin
 		end if;
 	end process;
 		
-	
-	p_bits:process(RST_I, CLK_I, CLKEN_I)
+	p_clk:process(CLK_I, CLKEN_I)
 	begin
-		if RST_I = '1' then
-			r_bit_state <= phase3;
-			SPI_nCS_o <= '1';
-			SPI_DAT_o <= '0';
-			SPI_CLK_o <= '0';
-		elsif rising_edge(CLK_I) then
-			if CLKEN_I = '1' then
-				
+		if rising_edge(CLK_I) then
+			if CLKEN_I = '1' then				
 				case r_bit_state is
 					when phase0 =>
 						SPI_CLK_o <= '0';
-						if r_row_state = start then
-							SPI_nCS_o <= '1';
-						else
-							SPI_nCS_o <= '0';
-						end if;
-						SPI_DAT_o <= sr(sr'high);
 						r_bit_state <= phase1;
 					when phase1 =>
 						r_bit_state <= phase2;
@@ -218,10 +222,31 @@ begin
 						r_bit_state <= phase0;
 					when others => 
 						r_bit_state <= phase3;
-						SPI_nCS_o <= '1';
-						SPI_DAT_o <= '0';
-						SPI_CLK_o <= '0';
 				end case;
+			end if;
+		end if;
+
+	end process;
+
+	
+	p_bits:process(RST_I, CLK_I, CLKEN_I)
+	begin
+		if RST_I = '1' then
+			SPI_nCS_o <= '1';
+			SPI_DAT_o <= '1';
+		elsif rising_edge(CLK_I) then
+			if CLKEN_I = '1' then
+				
+				if r_bit_state = phase0 then
+					if r_state = init or r_state = run then
+						if r_row_state = start then
+							SPI_nCS_o <= '1';
+						else
+							SPI_nCS_o <= '0';
+						end if;
+						SPI_DAT_o <= sr(sr'high);
+					end if;
+				end if;
 			end if;
 		end if;
 
